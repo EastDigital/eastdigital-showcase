c8ba2d3d144bcc17cc9e6bf6f82c66f4d01c508b
EastDigital <eastdigitalcompany@gmail.com>
2025-10-03 15:04:34 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -162,7 +162,7 @@ export default function Contact() {
                          </div>
                         <div>
                           <h3 className="font-semibold text-foreground">Phone</h3>
-                          <p className="text-muted-foreground">+91-99105 68689</p>
+                          <p className="text-muted-foreground">+91-9005 550773</p>
                         </div>
                       </div>
                     </div>
